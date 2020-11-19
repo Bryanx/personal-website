130e1a80e000b6984cdf323cb1fd699fc2525475
Bryan de Ridder <br.deridder@gmail.com>
2020-11-19 15:51:50 +0100

    Add SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { NotionRenderer, BlockMapType } from "react-notion";
+import { NextSeo } from "next-seo";
 import Head from "next/head";
 import Link from "next/link";
 import fetch from "node-fetch";
@@ -18,6 +19,23 @@ export async function getStaticProps() {
 
 const Home = ({ blockMap }) => (
   <div>
+    <NextSeo
+      title={"Bryan de Ridder"}
+      description="Hi, I'm Bryan! I like software programming and designing user interfaces."
+      openGraph={{
+        images: [
+          {
+            url: 'https://avatars0.githubusercontent.com/u/17814185?s=460&u=3bf382ad4310fbbc7daef4a52ce05ade816459b3&v=4',
+            width: 450,
+            height: 450,
+            alt: 'Bryan de Ridder',
+          }],
+      }}
+      twitter={{
+        handle: "@bryan_de_ridder",
+        cardType: "summary_large_image",
+      }}
+    />
     <Head>
       <style>{`body { margin: 0;}`}</style>
       <title>Bryan de Ridder</title>
