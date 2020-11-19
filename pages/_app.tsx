1cadc972cea7109f02c1c61edff294dadffa0b3c
Bryan de Ridder <br.deridder@gmail.com>
2020-11-19 09:25:02 +0100

    Add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import "react-notion/src/styles.css";
 import "prismjs/themes/prism-tomorrow.css";
 import React from "react";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
     <div>
+      <Head>
+        <link rel="icon" href="/public/favicon.svg" sizes="any" type="image/svg+xml" />
+      </Head>
       <Component {...pageProps} />
     </div>
   );
